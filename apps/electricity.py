0bfe400ab82f1a492e8298b04a2566264696a64c
Anas Kouri <81918135+anaskouri@users.noreply.github.com>
2022-09-21 14:23:21 -0400

    Update electricity.py

--- apps/electricity.py
+++ apps/electricity.py
@@ -177,6 +177,7 @@ layout = html.Div([
         dbc.Tab([
             html.Ul([
                 html.Br(),
+                html.Li('Created by: Anas Kouri'),   
                 html.Li('Temporal Coverage: 1985 - 2021'),
                 html.Li([
                     'Source: ',
